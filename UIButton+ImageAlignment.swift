cce61f6c5cae21e02308f071e0250112c89b7e83
changcun <qianchangcun@changba.com>
2018-02-26 17:24:27 +0800

    Fix contentHorizontalAlignment issue

--- UIButton+ImageAlignment.swift
+++ UIButton+ImageAlignment.swift
@@ -4,46 +4,108 @@
 //
 //  Created by changcun on 20/11/2017.
 //  Copyright © 2017 com.qcc. All rights reserved.
-//
+//  swiftlint:disable line_length
+//  swiftlint:disable function_body_length
+//  swiftlint:disable cyclomatic_complexity
+//  swiftlint:disable legacy_constructor
 
 import UIKit
 
-enum ImageAlignment: UInt{
+enum ImageAlignment: UInt {
     case left, right, top, bottom, centerUp, centerDown
 }
 
-extension UIButton
-{
+extension UIButton {
     func setPadding(padding: CGFloat, imageAlignment: ImageAlignment = .left) {
-        
         guard imageView?.image != nil && titleLabel?.text != nil else { return }
         guard let imageSize = imageView?.image?.size, let titleSize = self.titleLabel?.text?.size(withAttributes: [NSAttributedStringKey.font: titleLabel?.font ?? UIFont.systemFont(ofSize: 14.0)])
             else { return }
 
         titleEdgeInsets = UIEdgeInsets.zero
         imageEdgeInsets = UIEdgeInsets.zero
-        
+
         switch imageAlignment {
         case .left:
-            titleEdgeInsets = UIEdgeInsetsMake(0,
-                                               padding * 0.5,
-                                               0,
-                                               -padding * 0.5)
-            
-            imageEdgeInsets = UIEdgeInsetsMake(0,
-                                               -padding * 0.5,
-                                               0,
-                                               padding * 0.5)
+
+            switch contentHorizontalAlignment {
+            case .left, .leading:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   padding,
+                                                   0,
+                                                   -padding)
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   0,
+                                                   0,
+                                                   0)
+            case .center:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   padding * 0.5,
+                                                   0,
+                                                   -padding * 0.5)
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -padding * 0.5,
+                                                   0,
+                                                   padding * 0.5)
+            case .right, .trailing:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   0,
+                                                   0,
+                                                   0)
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -padding,
+                                                   0,
+                                                   padding)
+            default:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   padding * 0.5,
+                                                   0,
+                                                   -padding * 0.5)
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -padding * 0.5,
+                                                   0,
+                                                   padding * 0.5)
+            }
+
         case .right:
-            titleEdgeInsets = UIEdgeInsetsMake(0,
-                                               -(imageSize.width + padding * 0.5),
-                                               0,
-                                               (imageSize.width + padding * 0.5))
-            
-            imageEdgeInsets = UIEdgeInsetsMake(0,
-                                               titleSize.width + padding * 0.5,
-                                               0,
-                                               -(titleSize.width + padding * 0.5))
+
+            switch contentHorizontalAlignment {
+            case .left, .leading:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -(imageSize.width),
+                                                   0,
+                                                   (imageSize.width))
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   titleSize.width + padding,
+                                                   0,
+                                                   -(titleSize.width + padding))
+            case .center:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -(imageSize.width + padding * 0.5),
+                                                   0,
+                                                   (imageSize.width + padding * 0.5))
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   titleSize.width + padding * 0.5,
+                                                   0,
+                                                   -(titleSize.width + padding * 0.5))
+            case .right, .trailing:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -(imageSize.width + padding),
+                                                   0,
+                                                   (imageSize.width + padding))
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   titleSize.width,
+                                                   0,
+                                                   -(titleSize.width))
+            default:
+                titleEdgeInsets = UIEdgeInsetsMake(0,
+                                                   -(imageSize.width + padding * 0.5),
+                                                   0,
+                                                   (imageSize.width + padding * 0.5))
+                imageEdgeInsets = UIEdgeInsetsMake(0,
+                                                   titleSize.width + padding * 0.5,
+                                                   0,
+                                                   -(titleSize.width + padding * 0.5))
+            }
         case .top:
              titleEdgeInsets = UIEdgeInsetsMake(0.0,
                                                 -imageSize.width,
